929e57690896a5b08c0c6f9fda19baa845407906
Roman373 <roman.izibaeff2017@yandex.ru>
2021-10-13 20:26:19 +0500

    <a>

--- car/views.py
+++ car/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class MainPage(View):
+    def get(self, request):
+
+        context = {
+        }
+
+        return render(request, 'index.html', context=context)
