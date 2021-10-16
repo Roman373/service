f6e64e5970777f121692552a3b13d56b88bee441
Roman373 <roman.izibaeff2017@yandex.ru>
2021-10-16 22:27:58 +0500

    <b>

--- car/forms.py
+++ car/forms.py
@@ -19,3 +19,55 @@ class LoginForm(Form):
                 'placeholder': "Ваш пароль"
             })
         }
+
+
+class EnterForm(Form):
+    class Meta:
+        fields = [ 'login', 'password']
+
+        widgets = {
+            "login": TextInput(attrs={
+                'placeholder': "Ваш логин"
+            }),
+            "password": PasswordInput(attrs={
+                'placeholder': "Ваш пароль"
+            })
+        }
+
+
+class AppointmentForm(Form):
+    class Meta:
+        fields = ['name', 'phone', 'car','data']
+
+        widgets = {
+            "name": TextInput(attrs={
+                'placeholder': "Ваше имя"
+            }),
+            "phone": NumberInput(attrs={
+                'placeholder': "Ваш логин",
+                'value': '+7'
+            }),
+            "car": TextInput(attrs={
+                'placeholder': "Ваш автомобиль"
+            }),
+            "data": DateInput(attrs={
+            })
+        }
+
+
+class OrderphoneForm(Form):
+    class Meta:
+        fields = ['surname', 'name', 'phone']
+
+        widgets = {
+            "surname": TextInput(attrs={
+                'placeholder': "Ваша фамилия"
+            }),
+            "name": TextInput(attrs={
+                'placeholder': "Ваше имя"
+            }),
+            "phone": TextInput(attrs={
+                'placeholder': "Ваш логин",
+                'value': "+7"
+            })
+        }
